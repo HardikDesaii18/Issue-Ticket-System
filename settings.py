b2c51a3ad90cd96c81ad1bf816f8f474c87d4099
Hardik Desai <hardikdesai095@gmail.com>
2018-04-14 16:34:18 +0530

    Created a tornado web server

--- settings.py
+++ settings.py
@@ -1,9 +1,16 @@
 settings = dict(
     db=dict(
-        username='user-name',
-        password='password',
-        host='your-endpoint',
-        database='db-name',
-        port='0000'
-    )
+        username='hardikdesai',
+        password='Pulsar@2729',
+        host='postgresinstance.cxjyukbuyrhv.us-east-1.rds.amazonaws.com',
+        database='issue_ticket',
+        port='5432'
+    ),
+    tornado=dict(
+        debug=True,
+    ),
+    # todo CHECK WHAT DO IS changes
+    tornado_server_settings = {
+        "xheaders" : False
+    },
 )
