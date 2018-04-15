df5eee391d7d514652b08b3946218e39ec2a495a
Hardik Desai <hardikdesai095@gmail.com>
2018-04-15 12:05:43 +0530

    Created ticket handlers.

--- ticket/routes.py
+++ ticket/routes.py
@@ -1,4 +1,7 @@
+from ticket import handlers
+
 def get_app_routes():
     return [
-
+        (r"/api/ticket", handlers.CreteTicketHandler),
+        (r"/api/ticket/([-0-9a-fA-F]*)", handlers.TicketHandler)
     ]
